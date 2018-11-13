8bc540361bb0a733c63212cdb2c2c030d4e70408
msumair-nisum-com <msumair@nisum.com>
2018-11-13 17:19:04 +0500

    routing added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
-import { Row, Col } from "antd";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-import Login from "./components/Login";
+import Login from "./pages/login/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Row className="app" type="flex" justify="center" align="middle">
-          <Col>
-            <Login />
-          </Col>
-        </Row>
-      </div>
+        <Router>
+            <Switch>
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/" render={() => (<Redirect to="/login" />)} />
+            </Switch>
+        </Router>
     );
   }
 }
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Dashboard extends Component {
   state = {};
   render() {
-    return <div />;
+    return <div>Dashboard</div>;
   }
 }
 
