996d8c8fae912d53724b3ef3c8035e4c0c15912e
Hammad Tariq <hammad.tariq@macys.com>
2018-11-13 14:43:57 +0500

    ant lib added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Login from "./components/Login";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">Collaboration Tool</header>
+        <Login />
       </div>
     );
   }
