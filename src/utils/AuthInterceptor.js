6113567942b440b0e921534e2f24c9f88c89fda2
Hammad Tariq <hammad.tariq@macys.com>
2018-11-28 21:05:09 +0500

    production build config added

--- src/utils/AuthInterceptor.js
+++ src/utils/AuthInterceptor.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "universal-cookie";
 import { TOKEN, USER } from "../utils/Constant";
 
-// const BASE_URL = 'https://dev-107244-admin.oktapreview.com/api/v1';
-const BASE_URL = "http://localhost:3333";
+const BASE_URL =
+  window.BASE_URL || process.env.REACT_APP_DEV_API_URL || "http://localhost:80";
 
 var cookies = new Cookies();
 // Add a request interceptor
